084204feafe7efdf5e130285caebbbeff0b81d77
Roberto C. Rivera <rivera.roberto89@gmail.com>
2014-03-14 13:49:39 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NydiasCatering::Application.config.secret_key_base = '3d3c34dd2232af5c75a25cc6d41b4a62d212f89656f7d8f773c707bfd0e325902a0401c9b942cfa07017a83bd2dce696cea6432c2b8386bca893e659ff03b9ab'
